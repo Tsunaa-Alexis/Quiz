dd9793d1b6b4594e92c17fc5bbc63a1256a6fc14
Alexis <55393916+Tsunaa-Alexis@users.noreply.github.com>
2020-08-09 19:22:08 +0200

    suite

--- mooc-symfony4/src/Controller/AdvertController.php
+++ mooc-symfony4/src/Controller/AdvertController.php
@@ -4,13 +4,43 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
+/**
+ * @Route("/advert")
+ */
 class AdvertController
 {
-  public function index()
+  /**
+   * @Route( name="oc_advert_index")
+   */
+  public function index(Environment $twig)
   {
-    $content = "Notre propre Hello World !";
+    $content = $twig->render('Advert/index.html.twig');
 
     return new Response($content);
+  }
+
+  /**
+   * @Route("/view/{id}", name="oc_advert_view")
+   */
+  public function view($id)
+  {
+    // $id vaut 5 si l'URL appelée est /advert/view/5
+    
+    return new Response("Affichage de l'annonce d'id : ".$id);
+  }
+
+  /**
+   * @Route("/view/{year}/{slug}.{format}", name="oc_advert_view_slug", requirements={
+   *   "year"   = "\d{4}",
+   *   "format" = "html|xml"
+   * }, defaults={"format" = "html"})
+   */
+  public function viewSlug($slug, $year, $format)
+  {
+    return new Response("On pourrait afficher l'annonce correspondant au
+    slug '".$slug."', créée en ".$year." et au format ".$format.".");
   }
 }
